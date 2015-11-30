676732acf3fa89779b76de975f1bbf7b479e7c6a
Jaspal Sandhu <jss24@sfu.ca>
2015-11-30 13:22:12 -0800

    Switched to using LS read/write commands. Need to fix struct size.

--- nxtMapper.c
+++ nxtMapper.c
@@ -148,6 +148,76 @@ typedef struct {
 	uint8_t calibratedValue2;
 }__attribute__((packed)) sensorResponse;
 
+typedef struct {
+	uint8_t lsb;
+	uint8_t msb;
+	uint8_t response;
+	uint8_t command;
+	uint8_t port;
+}__attribute__((packed)) lsGetStatus;
+
+typedef struct {
+	uint8_t lsb;
+	uint8_t msb;
+	uint8_t response1;
+	uint8_t response2;
+	uint8_t status;
+	uint8_t bytesReady;
+}__attribute__((packed)) lsGetStatusResponse;
+
+typedef struct {
+	uint8_t lsb;
+	uint8_t msb;
+	uint8_t response;
+	uint8_t command;
+	uint8_t port;
+	uint8_t txDataLength;
+	uint8_t rxDataLength;
+	uint8_t dataSent1;
+	uint8_t dataSent2;
+}__attribute__((packed)) lsWrite;
+
+typedef struct {
+	uint8_t lsb;
+	uint8_t msb;
+	uint8_t response;
+	uint8_t response2;
+	uint8_t status;
+}__attribute__((packed)) lsWriteResponse;
+
+typedef struct {
+	uint8_t lsb;
+	uint8_t msb;
+	uint8_t response;
+	uint8_t command;
+	uint8_t port;
+}__attribute__((packed)) lsRead;
+
+typedef struct {
+	uint8_t lsb;
+	uint8_t msb;
+	uint8_t response;
+	uint8_t command;
+	uint8_t status;
+	uint8_t bytesRead1;
+	uint8_t bytesRead2;
+	uint8_t bytesRead3;
+	uint8_t bytesRead4;
+	uint8_t bytesRead5;
+	uint8_t bytesRead6;
+	uint8_t bytesRead7;
+	uint8_t bytesRead8;
+	uint8_t bytesRead9;
+	uint8_t bytesRead10;
+	uint8_t bytesRead11;
+	uint8_t bytesRead12;
+	uint8_t bytesRead13;
+	uint8_t bytesRead14;
+	uint8_t bytesRead15;
+	uint8_t bytesRead16;
+	uint8_t bytesRead17;
+}__attribute__((packed)) lsReadReturn;
+
 void activatePin() {
     FILE *joystickFile = fopen("/sys/class/gpio/export", "w");
     if (joystickFile == NULL) {
@@ -384,59 +454,180 @@ void nxtMove(int value) {
 
 }
 
+#define SENSOR_PORT 0x02
 void *EyeSensor(void* arg) {
 	printf("Started eye sensor thread!\n");
-	sensorResponse eyeRead = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	//sensorResponse eyeRead = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	setSensor eyeWrite = {
 		0x05, // LSB
         0x00, // MSB
         
-        0x80, // Command: Direct command requires no response
+        0x00, // Command: Direct command requires no response
         0x05, // Command Type: Set Input Mode
-        0x01, // Sensor Port: Port 1
+        SENSOR_PORT, // Sensor Port: Port 1
         0x0B, // Sensor Type: Lowspeed 9V
         0x00  // Sensor Mode: Raw
 	};
 	//commandResponse commandRes;
-	getSensor getResponse = {
+	/*getSensor getResponse = {
 		0x03, // LSB
 		0x00, // MSB
 		
 		0x00, // Command: Direct command require response
 		0x07, // Command Type: Get Input Values
-		0x01  // Sensor Port: Port 1
+		SENSOR_PORT  // Sensor Port: Port 1
+	};*/
+	
+	lsRead ls_read = {
+		0x03,
+		0x00,
+		
+		0x00,
+		0x10,
+		SENSOR_PORT
 	};
+	lsReadReturn lsReadResponse;
+	
+	lsWrite lsWriteCmd = {
+		0x07,
+		0x00,
+		
+		0x00, // Command Type
+		0x0F, // Command
+		SENSOR_PORT, // Port
+		0x02, // Bytes to write
+		0x01, // Bytes to read
+		0x02, // I2C address of device
+		0x42  // Address of register
+	};
+	
+	/*lsGetStatus lsStatus = {
+		0x03,
+		0x00,
+		
+		0x00,
+		0x0E,
+		SENSOR_PORT
+	};
+	lsGetStatusResponse lsStatusResponse;*/
 	
 	// Turn on sensor
 	if(write(s, (const void *)&eyeWrite, (int)sizeof(eyeWrite))) {
 		printf("Sensor turned on sucessfully!\n\n");
 	}
 	else {
-		printf("Sensor not turned on! Requires foreplay!\n\n");
+		printf("Sensor not turned on! Requires more foreplay!\n\n");
 	}
 	
-	while(1){
-	
-		sleep(1);
-		
-		// Poll sensor
-		if(write(s, (const void *)&getResponse, (int)sizeof(getResponse))) {
+	while(1) {
+		// Write to sensor that you want to read
+		if(write(s, (const void *)&lsWriteCmd, (int)sizeof(lsWriteCmd))) {
 			printf("Sensor write sucessful!\n\n");
 		}
 		else {
 			printf("Sensor write did not work!\n\n");
 		}
+	
+		sleep(1); // wait for sensor
 		
-		// Read sensor
-		printf("Attempting read\n");
-		if(read(s, (void*)&eyeRead, (int)sizeof(eyeRead))) {
+		
+		// Actually read the sensor
+		if(write(s, (const void *)&ls_read, (int)sizeof(ls_read))) {
 			printf("Sensor read sucessful!\n\n");
 		}
 		else {
 			printf("Sensor read did not work!\n\n");
 		}
 		
+		// Store the reading into the read struct
+		printf("Attempting read\n");
+		if(read(s, (void*)&lsReadResponse, (int)sizeof(lsReadResponse))) {
+			printf("Read into struct sucessful!\n\n");
+		}
+		else {
+			printf("Read into struct did not work!\n\n");
+		} 
+		
+	
+	
+	/*sleep(1);
+		
+		if(write(s, (void*)&lsStatus, (int)sizeof(lsStatus))) {
+			printf("Sensor read sucessful!\n\n");
+		}
+		else {
+			printf("Sensor read did not work!\n\n");
+		}
+		
+		sleep(1);
+		
+		
+		
+		if(read(s, (void*)&lsStatusResponse, (int)sizeof(lsStatusResponse))) {
+			printf("Sensor read sucessful!\n\n");
+		}
+		else {
+			printf("Sensor read did not work!\n\n");
+		}
+	
+	// Print lsStatusResponse
+	printf("%x\n %x\n %x\n %x\n %x\n %x\n",
+	lsStatusResponse.lsb,
+	lsStatusResponse.msb,
+	lsStatusResponse.response1,
+	lsStatusResponse.response2,
+	lsStatusResponse.status,
+	lsStatusResponse.bytesReady);*/
+	
+// Print lsReadResponse	
 		printf("\
+LSB:         %x\n \
+MSB:         %x\n \
+0x02:        %x\n \
+0x07:        %x\n \
+Status:      %x\n \
+Input Port:  %x\n \
+Valid:       %x\n \
+Calibrated:  %x\n \
+Sensor Type: %x\n \
+Sensor Mode: %x\n \
+RAW:         %x\n \
+RAW:         %x\n \
+Normalized:  %x\n \
+Normalized:  %x\n \
+Scaled:      %x\n \
+Scaled:      %x\n \
+Calibrated:  %x\n \
+Calibrated:  %x\n \
+Calibrated:  %x\n \
+Calibrated:  %x\n \
+Calibrated:  %x\n \
+Calibrated:  %x\n",
+		lsReadResponse.lsb,
+		lsReadResponse.msb,
+		lsReadResponse.response,
+		lsReadResponse.command,
+		lsReadResponse.status,
+		lsReadResponse.bytesRead1,
+		lsReadResponse.bytesRead2,
+		lsReadResponse.bytesRead3,
+		lsReadResponse.bytesRead4,
+		lsReadResponse.bytesRead5,
+		lsReadResponse.bytesRead6,
+		lsReadResponse.bytesRead7,
+		lsReadResponse.bytesRead8,
+		lsReadResponse.bytesRead9,
+		lsReadResponse.bytesRead10,
+		lsReadResponse.bytesRead11,
+		lsReadResponse.bytesRead12,
+		lsReadResponse.bytesRead13,
+		lsReadResponse.bytesRead14,
+		lsReadResponse.bytesRead15,
+		lsReadResponse.bytesRead16,
+		lsReadResponse.bytesRead17);
+	
+// Print eyeRead		
+		/*printf("\
 LSB:         %x\n \
 MSB:         %x\n \
 0x02:        %x\n \
@@ -472,7 +663,7 @@ Calibrated:  %x\n",
 		eyeRead.scaledValue1,
 		eyeRead.scaledValue2,
 		eyeRead.calibratedValue1,
-		eyeRead.calibratedValue2);
+		eyeRead.calibratedValue2);*/
 		//printf("Raw: (%x, %x)", eyeRead.rawValue1, eyeRead.rawValue2);
 	/*printf("%x\n %x\n %x\n %x\n %x\n", commandRes.lsb, commandRes.msb, commandRes.response, commandRes.command, commandRes.status);*/
 	}
